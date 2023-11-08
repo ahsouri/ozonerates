d566a2239a7a10dd4e9c73130e0fd9e2ec01e765
Amir Souri <ahsouri@gmail.com>
2023-11-08 11:51:05 -0500

    fix job.py

--- run/job.py
+++ run/job.py
@@ -26,9 +26,10 @@ ozonerates_obj = ozonerates()
 sat_path = []
 sat_path.append(Path(sat_dir_no2))
 sat_path.append(Path(sat_dir_hcho))
-ozonerates_obj.read_data('GMI', Path('ctm_dir'),
+ozonerates_obj.read_data('GMI', Path(ctm_dir),
                              sat_path, str(year) + f"{month:02}",output_folder = output_nc_inputparam_dir,
                              read_ak=False, trop=True, num_job=num_job)
 ozonerates_obj.po3estimate_empirical(
            output_nc_inputparam_dir, output_nc_inputparam_dir, str(year) + '-' + f"{month:02}" +
            '-01', str(year) + '-' + f"{month+1:02}" + '-01')
+ozonerates_obj.reporting("PO3_estimates_" + str(year) + f"{month:02}",folder=output_pdf_dir)
